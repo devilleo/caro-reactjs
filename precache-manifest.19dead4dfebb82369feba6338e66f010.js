ec5767b702c33bf36ebc4677e9390fb551ef83d3
Kha Leo <lexuankha2409@gmail.com>
2019-10-09 21:03:35 +0700

    Updates

---
+++ precache-manifest.19dead4dfebb82369feba6338e66f010.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed92e5f873891066d7f2466247ebb6f",
+    "url": "/caro-reactjs/index.html"
+  },
+  {
+    "revision": "288fb8159e3f962277a2",
+    "url": "/caro-reactjs/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "98b80ac3564abe8e34ba",
+    "url": "/caro-reactjs/static/css/main.ea574fc5.chunk.css"
+  },
+  {
+    "revision": "288fb8159e3f962277a2",
+    "url": "/caro-reactjs/static/js/2.2bee1ade.chunk.js"
+  },
+  {
+    "revision": "98b80ac3564abe8e34ba",
+    "url": "/caro-reactjs/static/js/main.19ee50ef.chunk.js"
+  },
+  {
+    "revision": "7b282da03cb1dc4eba2e",
+    "url": "/caro-reactjs/static/js/runtime~main.89d2865a.js"
+  },
+  {
+    "revision": "553f0afcc24dfe5f647475e23c9c124b",
+    "url": "/caro-reactjs/static/media/background-board.553f0afc.jpg"
+  }
+]);
